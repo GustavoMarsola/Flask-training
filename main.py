2db16feab019435620c3d659328b5b680aa1383d
Gustavo Marsola <86449687+GustavoMarsola@users.noreply.github.com>
2022-11-15 12:23:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def initial_page():
+    user = request.form.get('email')
+    password = request.form.get('password')
+    print(user,password)
     return render_template('index.html')
 
 if __name__ == '__main__':
