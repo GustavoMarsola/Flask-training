174cee6f32fb057a0243368eadaf4ec2486a3130
Gustavo Marsola <86449687+GustavoMarsola@users.noreply.github.com>
2022-11-15 14:44:33 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, abort
+from connection import Database
 
 app = Flask(__name__)
+db = Database()
 
 @app.route('/', methods=['GET','POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('email')
+        password = request.form.get('password')
+        verify = db.compare(user,password)
+        if verify is True:
+            return redirect(url_for('initial_page'), code=302)
+        else: 
+            abort(401)
+    return render_template('login.html')
+
+@app.route('/initial_page')
 def initial_page():
-    user = request.form.get('email')
-    password = request.form.get('password')
-    print(user,password)
-    return render_template('index.html')
+    return render_template('initial_page.html')
 
 if __name__ == '__main__':
     app.run()
