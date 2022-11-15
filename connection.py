fda3caa0b88388a4c81239c6aa9f0b3d857ee228
Gustavo Marsola <86449687+GustavoMarsola@users.noreply.github.com>
2022-11-15 14:11:34 -0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -1,1 +1,16 @@
-import tinydb as db
+from tinydb import TinyDB, Query
+
+class Database:
+
+    def __init__(self):
+        self.db = TinyDB('db.json')
+        self.user = Query()
+    
+    def compare(self, email,password):
+        locate = self.db.search(self.user.email == email)
+        if len(locate) == 0:
+            return "Email não cadastrado"
+        senha = locate[0]['senha']
+        if senha != password:
+            return "Senha incorreta"
+        return senha
